e6d918be1fc3baa1010f0738ee7fcff4ba4c79a4
inming <yinming2100@pku.edu.cn>
2017-08-24 14:45:20 +0800

    new
    add  a label

--- anfis/backtest.py
+++ anfis/backtest.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import talib
 import numpy as np
+import os
+
+path = os.getcwd()+'\\'
 
 # 读取数据
-Data = pd.read_table('C:/Users/szc_ghost/Documents/GitHub/anfis_YS/999999.txt', delim_whitespace=True, encoding='gbk')
+Data = pd.read_table(path+'res\\999999.txt', delim_whitespace=True, encoding='gbk')
 Data = Data[:-1]
 Data.columns = ['time', 'openp', 'highp', 'lowp', 'closep', 'volume', 'amount']
 
@@ -11,12 +14,21 @@ Data.columns = ['time', 'openp', 'highp', 'lowp', 'closep', 'volume', 'amount']
 # 计算指标，汇总到indicators里
 def myMACD(price, fastperiod=12, slowperiod=26, signalperiod=9):
     ewma12 = pd.ewma(price, span=fastperiod)
-    ewma60 = pd.ewma(price, span=slowperiod)
-    dif = ewma12 - ewma60
+    ewma26 = pd.ewma(price, span=slowperiod)
+    dif = ewma12 - ewma26
     dea = pd.ewma(dif, span=signalperiod)
     bar = (dif - dea)  # 有些地方的bar = (dif-dea)*2，但是talib中MACD的计算是bar = (dif-dea)*1
     return dif, dea, bar
 
+#添加数据标签
+def getLabel(x):
+    if x == 0:
+        return 0
+    else:
+        return int(x/abs(x))
+
+label = list(map(getLabel,np.diff(Data['closep'])))
+label = [0]+label
 
 MACD, signal, hist = talib.MACD(Data['closep'].values, fastperiod=12, slowperiod=26, signalperiod=9)
 DIF, DEA, BAR = myMACD(Data['closep'].values, fastperiod=12, slowperiod=26, signalperiod=9)
@@ -25,4 +37,6 @@ K, D = talib.STOCH(Data['highp'].values, Data['lowp'].values,
                    Data['closep'].values, fastk_period=14, slowk_period=3,
                    slowk_matype=0, slowd_period=3, slowd_matype=0)
 RSI = talib.RSI(Data['closep'].values, timeperiod=14)
-indicators = np.column_stack((MACD, DIF, DEA, EMA5, K, D, RSI))
+indicators = np.column_stack((MACD, DIF, DEA, EMA5, K, D, RSI,label))[35:]
+
+np.savetxt(path+'res\\indicators.txt',indicators,delimiter=',',fmt='%.3f')
--- anfis/tests.py
+++ anfis/tests.py
@@ -2,11 +2,13 @@ import anfis
 import membership.mfDerivs
 import membership.membershipfunction
 #sfwefwefwfj
-import numpy
+import numpy as np
+import pandas as pd
 
-ts = numpy.loadtxt("trainingSet.txt", usecols=[1,2,3])#numpy.loadtxt('c:\\Python_fiddling\\myProject\\MF\\trainingSet.txt',usecols=[1,2,3])
-X = ts[:,0:2]
-Y = ts[:,2]
+ts = np.loadtxt("res\\indicators.txt",delimiter=',')#numpy.loadtxt('c:\\Python_fiddling\\myProject\\MF\\trainingSet.txt',usecols=[1,2,3])
+
+X = ts[:,:2]
+Y = ts[:,-1]
 
 mf = [[['gaussmf',{'mean':-11.,'sigma':5.}],['gaussmf',{'mean':-8.,'sigma':5.}],['gaussmf',{'mean':-14.,'sigma':20.}],['gaussmf',{'mean':-7.,'sigma':7.}]],
             [['gaussmf',{'mean':-10.,'sigma':20.}],['gaussmf',{'mean':-20.,'sigma':11.}],['gaussmf',{'mean':-9.,'sigma':30.}],['gaussmf',{'mean':-10.5,'sigma':5.}]]]
